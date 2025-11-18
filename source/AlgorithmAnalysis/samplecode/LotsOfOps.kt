32a3f1c2833a7e1d2c393dba7d40606d079bd05a
Dave Musicant (limbo) <dmusicant-limbo@carleton.edu>
2025-11-18 14:11:09 -0600

    Big O Notation updated

---
+++ source/AlgorithmAnalysis/samplecode/LotsOfOps.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val a = 5
+    val b = 6
+    val c = 10
+    val n = 1000
+    for (i in 0..<n) {
+        for (j in 0..<n) {
+            val x = i * i
+            val y = j * j
+            val z = i * j
+        }
+    }
+
+    for (k in 0..<n) {
+        val w = a * k + 45
+        val v = b * b
+    }
+    val d = 33
+}
