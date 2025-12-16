2917f436d7c49bff3a669ee920b3f7382f42318e
Dave Musicant <dmusicant@carleton.edu>
2025-12-16 08:15:15 -0600

    Basic DS Implementing a Queue

---
+++ source/BasicDS/samplecode/src/main/kotlin/Queue.kt
@@ -0,0 +1,59 @@
+class Queue<T> {
+
+    /*
+     * The tail of the queue is at the beginning
+     * of the list; the head is the last item
+     */
+    private val items = mutableListOf<T>()
+
+    /*
+     * Returns true if there are no items in the queue;
+     * false otherwise.
+     */
+    fun isEmpty(): Boolean {
+        return items.isEmpty()
+    }
+
+    /*
+    * Add an item to the tail of the queue
+    */
+    fun enqueue(item: T) {
+        items.add(0, item)
+    }
+
+    /*
+     * Remove the item at the head of the queue and return it.
+     * If the queue is empty, throws an exception.
+     */
+    fun dequeue(): T {
+        if (isEmpty()) {
+            throw NoSuchElementException("Queue is empty.")
+        }
+        return items.removeLast()
+    }
+
+    /*
+     * Return the item at the head of the queue, but do not remove it.
+     * If the queue is empty, throws an exception.
+     */
+    fun peek(): T {
+        if (isEmpty()) {
+            throw NoSuchElementException("Queue is empty.")
+        }
+        return items[items.count() - 1]
+    }
+
+    /*
+     * Returns the number of items in the queue.
+     */
+    fun size(): Int {
+        return items.count()
+    }
+
+    /*
+     * Convert to string as an array from tail to head
+     */
+    override fun toString(): String {
+        return items.toString()
+    }
+}
---
+++ source/BasicDS/samplecode/src/main/kotlin/QueueTest.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val q = Queue<Int>()
+    println("isEmpty returns ${q.isEmpty()}")
+
+    q.enqueue(4)
+    q.enqueue(27)
+    println("Queue is now: $q")
+
+    q.enqueue(1066)
+    println("After another enqueue, size is ${q.size()}")
+    println("isEmpty now returns ${q.isEmpty()}")
+
+    q.enqueue(4711)
+
+    println("After another enqueue, queue is $q")
+
+    var head = q.dequeue()
+    println("Dequeue - head was: $head")
+    head = q.dequeue()
+    println("Dequeue again - head was: $head")
+
+    println("Queue is now: $q")
+    println("Size is now: ${q.size()}")
+}
