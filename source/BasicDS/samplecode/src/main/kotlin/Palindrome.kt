b53541a49b03bccab0368e6df89734865416ad9b
Dave Musicant <dmusicant@carleton.edu>
2025-12-16 13:17:02 -0600

    Basic DS Palindrome Checker

---
+++ source/BasicDS/samplecode/src/main/kotlin/Palindrome.kt
@@ -0,0 +1,31 @@
+fun isPalindrome(str: String): Boolean {
+    val charDeque = Deque<Char>()
+
+    for (ch in str) {
+        charDeque.addTail(ch)
+    }
+
+    while (charDeque.size() > 1) {
+        val first = charDeque.removeHead()
+        val last = charDeque.removeTail()
+        if (first != last) {
+            return false
+        }
+    }
+    return true
+}
+
+fun main() {
+    println(
+        "Is \"regular\" a palindrome? " +
+                isPalindrome("regular")
+    )
+    println(
+        "Is \"rotator\" a palindrome? " +
+                isPalindrome("rotator")
+    )
+    println(
+        "Is \"deed\" a palindrome? " +
+                isPalindrome("deed")
+    )
+}
