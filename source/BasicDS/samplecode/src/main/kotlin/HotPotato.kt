2f8629e4c71b1669925779529a55d6264c3ced51
Dave Musicant <dmusicant@carleton.edu>
2025-12-16 08:28:43 -0600

    Basic DS Hot Potato

---
+++ source/BasicDS/samplecode/src/main/kotlin/HotPotato.kt
@@ -0,0 +1,27 @@
+fun hotPotato(nameList: List<String>, num: Int): String {
+    val simQueue = Queue<String>()
+
+    for (name in nameList) {
+        simQueue.enqueue(name)
+    }
+
+    while (simQueue.size() > 1) {
+        // pass the potato: move person at head to tail
+        for (pass in 0..< num) {
+            simQueue.enqueue(simQueue.dequeue())
+        }
+
+        val removed = simQueue.dequeue() // remove person at head
+        println("$removed is out of the game.")
+    }
+    return simQueue.dequeue()
+}
+
+fun main() {
+    val people = listOf<String>(
+        "Bill", "David", "Susan", "Jane",
+        "Kent", "Brad"
+    )
+    val lastPerson = hotPotato(people, 7)
+    println("Last person is $lastPerson.")
+}
