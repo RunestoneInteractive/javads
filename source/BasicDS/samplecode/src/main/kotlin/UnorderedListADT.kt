6c361d7497d05c507230dba511cce9fb2baaab03
Dave Musicant <dmusicant@carleton.edu>
2025-12-17 13:22:31 -0600

    Basic DS Unordered List ADT

---
+++ source/BasicDS/samplecode/src/main/kotlin/UnorderedListADT.kt
@@ -0,0 +1,25 @@
+interface UnorderedListADT<T> {
+
+    // Add a new item to the end of the list.
+    fun add(item: T)
+
+    // Add a new item at the specified index.
+    fun add(index: Int, item: T)
+
+    // Return item at specified index.
+    fun get(index: Int): T
+
+    // Find index of first occurrence of item in list.
+    fun indexOf(item: T): Int
+
+    // Returns true if there are no items in the deque;
+    // false otherwise.
+    fun isEmpty(): Boolean
+
+    // Removes item at specified index. Items at higher
+    // index values are shifted down to fill the gap.
+    fun removeAt(index: Int)
+
+    // Returns the number of items in the list.
+    fun size(): Int
+}
