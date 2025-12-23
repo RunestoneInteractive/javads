30e1c44b737c9642f57f95f0f93b574f1fd0ff24
Dave Musicant <dmusicant@carleton.edu>
2025-12-23 16:19:11 -0600

    Introduction Getting Started with Data

---
+++ source/Introduction/samplecode/src/main/kotlin/ArrayNew.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val ages = Array<Int>(3) { 0 }
+    val prices = Array<Double>(4) { 0.0 }
+    val valid = Array<Boolean>(3) { false }
+    val words = Array<String>(3) { "" }
+}
---
+++ source/Introduction/samplecode/src/main/kotlin/ArrayOf.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val ages = arrayOf(32, 47, 19, 62)
+    val prices = arrayOf(12.95, 13.88, 10.66)
+    val words = arrayOf("ant", "bee", "cat", "dog", "elk")
+}
