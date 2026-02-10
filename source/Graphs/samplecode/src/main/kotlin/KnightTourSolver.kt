ca80fee109b9fdabc11e6c5982b9f16cd6529cdb
Dave Musicant <dmusicant@carleton.edu>
2026-02-10 16:15:51 -0600

    Knight's tour implementation cleaned up

---
+++ source/Graphs/samplecode/src/main/kotlin/KnightTourSolver.kt
@@ -0,0 +1,31 @@
+class KnightTourSolver(val graph: GraphADT<Int>, val startVertex: Int, val limit: Int) {
+    val tourPath = mutableSetOf<Int>()
+
+    init {
+        knightTour(startVertex)
+    }
+
+    private fun knightTour(newVertex: Int): Boolean {
+
+        tourPath.add(newVertex)
+
+        if (tourPath.count() == limit) {     // found a tour
+            return true
+        } else {
+            // Try each neighbor
+            val neighbors = graph.getNeighbors(newVertex)!!   // We know every node has a neighbor
+            for (neighbor in neighbors) {
+                if (neighbor !in tourPath) {
+                    val done = knightTour(neighbor)
+                    if (done) {
+                        return true
+                    }
+                }
+            }
+
+            // If got to here, no tour found; remove current and return false
+            tourPath.remove(newVertex)
+            return false
+        }
+    }
+}
--- source/Graphs/samplecode/src/main/kotlin/KnightTourTest.kt
+++ source/Graphs/samplecode/src/main/kotlin/KnightTourTest.kt
@@ -1,10 +1,8 @@
 fun main() {
     val size = 8
     val kg = buildKnightGraph(size)
-    val tourVertices = mutableSetOf<Int>()
-    val tourPath = mutableListOf<Int>()
-    val solutionPath = knightTour(kg, tourVertices, tourPath, 0, size*size)
-    println(solutionPath)
+    val solution = KnightTourSolver(kg, 0, size*size)
+r    println(solution.tourPath)
 
     // Print out a chessboard, so can look at locations
     for (row in size-1 downTo 0) {
