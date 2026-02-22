770243c6e35b5c30bf12ea661858ff151684e394
Dave Musicant <dmusicant@carleton.edu>
2026-02-21 18:04:14 -0600

    Graphs Dijkstra implementation updated

--- source/Graphs/samplecode/src/main/kotlin/Dijkstra.kt
+++ source/Graphs/samplecode/src/main/kotlin/Dijkstra.kt
@@ -1,50 +1,48 @@
-//class DijkstraSolver<V>(val graph: GraphADT<V>, val start: V) {
-//    val previous = mutableMapOf<V, V?>()
-//    val distance = mutableMapOf<V, Double>()
-//
-//    init {
-//        dijsktra(start)
-//    }
-//
-//    data class PqItem<V>(val vertex: V, val distance: Double):
-//            Comparable<PqItem<V>> {
-//
-//        override fun compareTo(other: PqItem<V>): Int {
-//            return (this.distance - other.distance).toInt()
-//        }
-//    }
-//
-//    private fun dijsktra(start: V) {
-//        // Initialize distances to all vertices
-//        for (vertex in graph.getVertices()) {
-//            distance[vertex] = Double.MAX_VALUE
-//        }
-//        distance[start] = 0.0
-//        previous[start] = null
-//
-//        // Insert into priority queue
-//        val pqItems = mutableListOf<PqItem<V>>()
-//        for (vertex in graph.getVertices()) {
-//            pqItems.add(PqItem(vertex, distance[vertex]!!))
-//        }
-//        val pq = BinaryHeapPriorityQueue(pqItems)
-//
-//        while (pq.size() > 0) {
-//            val (curVertex, curDistance) = pq.delete()!!
-//            val neighborsAndDistances =
-//                graph.getNeighborsAndWeights(curVertex)!!
-//            for ((neighborVertex, neighborDistance) in neighborsAndDistances) {
-//                val newDistance = curDistance + neighborDistance
-//                if (newDistance < distance[neighborVertex]!!) {
-//                    val oldDistance = distance[neighborVertex]!!
-//                    distance[neighborVertex] = newDistance
-//                    previous[neighborVertex] = curVertex
-//
-//                    // update priority
-//                    pq.removeElement(PqItem(neighborVertex, oldDistance))
-//                    pq.insert(PqItem(neighborVertex, newDistance))
-//                }
-//            }
-//        }
-//    }
-//}
+class DijkstraSolver<V>(val graph: GraphADT<V>, val start: V) {
+    val previous = mutableMapOf<V, V?>()
+    val distance = mutableMapOf<V, Double>()
+
+    init {
+        dijsktra(start)
+    }
+
+    data class PqItem<V>(val vertex: V, val distance: Double):
+            Comparable<PqItem<V>> {
+
+        override fun compareTo(other: PqItem<V>): Int {
+            return (this.distance - other.distance).toInt()
+        }
+    }
+
+    private fun dijsktra(start: V) {
+        // Initialize distances to all vertices
+        for (vertex in graph.getVertices()) {
+            distance[vertex] = Double.MAX_VALUE
+        }
+        distance[start] = 0.0
+        previous[start] = null
+
+        // Insert into priority queue
+        val pqItems = mutableListOf<PqItem<V>>()
+        for (vertex in graph.getVertices()) {
+            pqItems.add(PqItem(vertex, distance[vertex]!!))
+        }
+        val pq = BinaryHeapPriorityQueue(pqItems)
+
+        while (!pq.isEmpty()) {
+            val (curV, curDist) = pq.delete()!!
+            for (nbrV in graph.getNeighbors(curV)!!) {
+                val newDist = curDist + graph.getWeight(curV, nbrV)!!
+                if (newDist < distance[nbrV]!!) {
+                    val oldDist = distance[nbrV]!!
+                    distance[nbrV] = newDist
+                    previous[nbrV] = curV
+
+                    // update priority
+                    pq.removeElement(PqItem(nbrV, oldDist))
+                    pq.insert(PqItem(nbrV, newDist))
+                }
+            }
+        }
+    }
+}
--- source/Graphs/samplecode/src/main/kotlin/DijkstraTest.kt
+++ source/Graphs/samplecode/src/main/kotlin/DijkstraTest.kt
@@ -1,37 +1,18 @@
-//fun main() {
-//    val graph = AdjacencyListGraph<String>(directed = false)
-//    graph.addEdge("u", "x", 1.0)
-//    graph.addEdge("x", "u", 1.0)
-//
-//    graph.addEdge("u", "v", 2.0)
-//    graph.addEdge("v", "u", 2.0)
-//
-//    graph.addEdge("u", "w", 5.0)
-//    graph.addEdge("w", "u", 5.0)
-//
-//    graph.addEdge("v", "w", 3.0)
-//    graph.addEdge("w", "v", 3.0)
-//
-//    graph.addEdge("w", "z", 5.0)
-//    graph.addEdge("z", "w", 5.0)
-//
-//    graph.addEdge("y", "z", 1.0)
-//    graph.addEdge("z", "y", 1.0)
-//
-//    graph.addEdge("x", "y", 1.0)
-//    graph.addEdge("y", "x", 1.0)
-//
-//    graph.addEdge("v", "x", 2.0)
-//    graph.addEdge("x", "v", 2.0)
-//
-//    graph.addEdge("w", "x", 3.0)
-//    graph.addEdge("x", "w", 3.0)
-//
-//    graph.addEdge("w", "y", 1.0)
-//    graph.addEdge("y", "w", 1.0)
-//
-//    val dijstraSolution = DijkstraSolver(graph, "u")
-//    println(dijstraSolution.previous)
-//    println(dijstraSolution.distance)
-//
-//}
+fun main() {
+    val graph = AdjListGraph<String>(directed = false)
+    graph.addEdge("u", "x", 1.0)
+    graph.addEdge("u", "v", 2.0)
+    graph.addEdge("u", "w", 5.0)
+    graph.addEdge("v", "w", 3.0)
+    graph.addEdge("w", "z", 5.0)
+    graph.addEdge("y", "z", 1.0)
+    graph.addEdge("x", "y", 1.0)
+    graph.addEdge("v", "x", 2.0)
+    graph.addEdge("w", "x", 3.0)
+    graph.addEdge("w", "y", 1.0)
+
+    val dijstraSolution = DijkstraSolver(graph, "u")
+    println(dijstraSolution.previous)
+    println(dijstraSolution.distance)
+
+}
