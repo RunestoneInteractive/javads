183647ed1145a127419ad1af8744c3db48e691d1
Dave Musicant <dmusicant@carleton.edu>
2026-02-21 21:40:32 -0600

    Graphs Prim's algorithm

--- source/Graphs/samplecode/src/main/kotlin/Prim.kt
+++ source/Graphs/samplecode/src/main/kotlin/Prim.kt
@@ -1,56 +1,56 @@
-//class PrimSolver<V>(val graph: GraphADT<V>, val start: V) {
-//    //    private val previous = mutableMapOf<V, V?>()
-//    private val distance = mutableMapOf<V, Double>()
-//
-//    val mst = AdjacencyListGraph<V>(directed = false)
-//
-//    init {
-//        prim(start)
-//    }
-//
-//    data class PqItem<V>(val vertex: V, val distance: Double) :
-//        Comparable<PqItem<V>> {
-//
-//        override fun compareTo(other: PqItem<V>): Int {
-//            return (this.distance - other.distance).toInt()
-//        }
-//    }
-//
-//    private fun prim(start: V) {
-//        // Initialize distances to all vertices
-//        for (vertex in graph.getVertices()) {
-//            distance[vertex] = Double.MAX_VALUE
-//        }
-//        distance[start] = 0.0
-////        previous[start] = null
-//
-//        // Insert into priority queue
-//        val pqItems = mutableListOf<PqItem<V>>()
-//        for (vertex in graph.getVertices()) {
-//            pqItems.add(PqItem(vertex, distance[vertex]!!))
-//        }
-//        val pq = BinaryHeapPriorityQueue(pqItems)
-//
-//        while (pq.size() > 0) {
-//            val (curVertex, curDistance) = pq.delete()!!
-//            val neighborsAndDistances =
-//                graph.getNeighborsAndWeights(curVertex)!!
-//            for ((neighborVertex, neighborDistance) in neighborsAndDistances) {
-//                val newDistance = neighborDistance
-//                if ((neighborVertex !in mst.getVertices()) &&
-//                    (newDistance < distance[neighborVertex]!!)
-//                ) {
-//                    mst.addEdge(curVertex, neighborVertex, neighborDistance)
-//                    mst.addEdge(neighborVertex, curVertex, neighborDistance)
-//                    val oldDistance = distance[neighborVertex]!!
-//                    distance[neighborVertex] = newDistance
-////                    previous[neighborVertex] = curVertex
-//
-//                    // update priority
-//                    pq.removeElement(PqItem(neighborVertex, oldDistance))
-//                    pq.insert(PqItem(neighborVertex, newDistance))
-//                }
-//            }
-//        }
-//    }
-//}
+class PrimSolver<V>(val graph: GraphADT<V>, val start: V) {
+    val mst = AdjListGraph<V>(directed = false)
+
+    init {
+        prim(start)
+    }
+
+    data class PqItem<V>(val vertex: V, val distance: Double) :
+        Comparable<PqItem<V>> {
+
+        override fun compareTo(other: PqItem<V>): Int {
+            return (this.distance - other.distance).toInt()
+        }
+    }
+
+    private fun prim(start: V) {
+        val distance = mutableMapOf<V, Double>()
+        val previous = mutableMapOf<V, V?>()
+
+        // Initialize distances to all vertices
+        for (vertex in graph.getVertices()) {
+            distance[vertex] = Double.MAX_VALUE
+        }
+        distance[start] = 0.0
+        previous[start] = null
+
+        // Insert into priority queue
+        val pqItems = mutableListOf<PqItem<V>>()
+        for (vertex in graph.getVertices()) {
+            pqItems.add(PqItem(vertex, distance[vertex]!!))
+        }
+        val pq = BinaryHeapPriorityQueue(pqItems)
+
+        while (!pq.isEmpty()) {
+            val (curV, curDist) = pq.delete()!!
+            for (nbrV in graph.getNeighbors(curV)!!) {
+                val newDist = graph.getWeight(curV, nbrV)!!
+                if (nbrV !in previous && newDist < distance[nbrV]!!) {
+                    val oldDist = distance[nbrV]!!
+                    distance[nbrV] = newDist
+                    previous[nbrV] = curV
+
+                    // update priority
+                    pq.removeElement(PqItem(nbrV, oldDist))
+                    pq.insert(PqItem(nbrV, newDist))
+                }
+            }
+        }
+
+        for (vertex in previous.keys) {
+            if (previous[vertex] != null) {
+                mst.addEdge(vertex, previous[vertex]!!, distance[vertex]!!)
+            }
+        }
+    }
+}
--- source/Graphs/samplecode/src/main/kotlin/PrimTest.kt
+++ source/Graphs/samplecode/src/main/kotlin/PrimTest.kt
@@ -1,36 +1,14 @@
-//fun main() {
-//    val graph = AdjacencyListGraph<String>(directed=false)
-//    graph.addEdge("A", "u", 1.0)
-//    graph.addEdge("x", "u", 1.0)
-//
-//    graph.addEdge("u", "v", 2.0)
-//    graph.addEdge("v", "u", 2.0)
-//
-//    graph.addEdge("u", "w", 5.0)
-//    graph.addEdge("w", "u", 5.0)
-//
-//    graph.addEdge("v", "w", 3.0)
-//    graph.addEdge("w", "v", 3.0)
-//
-//    graph.addEdge("w", "z", 5.0)
-//    graph.addEdge("z", "w", 5.0)
-//
-//    graph.addEdge("y", "z", 1.0)
-//    graph.addEdge("z", "y", 1.0)
-//
-//    graph.addEdge("x", "y", 1.0)
-//    graph.addEdge("y", "x", 1.0)
-//
-//    graph.addEdge("v", "x", 2.0)
-//    graph.addEdge("x", "v", 2.0)
-//
-//    graph.addEdge("w", "x", 3.0)
-//    graph.addEdge("x", "w", 3.0)
-//
-//    graph.addEdge("w", "y", 1.0)
-//    graph.addEdge("y", "w", 1.0)
-//
-//    val primSolution = PrimSolver(graph, "u")
-//    println(primSolution.mst)
-//
-//}
+fun main() {
+    val graph = AdjListGraph<String>(directed=false)
+    graph.addEdge("A", "B", 2.0)
+    graph.addEdge("A", "C", 3.0)
+    graph.addEdge("B", "C", 1.0)
+    graph.addEdge("B", "D", 1.0)
+    graph.addEdge("B", "E", 4.0)
+    graph.addEdge("D", "E", 1.0)
+    graph.addEdge("C", "F", 5.0)
+    graph.addEdge("F", "G", 1.0)
+    val primSolution = PrimSolver(graph, "A")
+    println(primSolution.mst)
+
+}
